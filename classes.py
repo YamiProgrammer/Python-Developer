2d73514d93bf67400e40f958a4f5fc0781c4c85b
YamiProgrammer <ticmanclarence@gmail.com>
2025-02-09 00:40:02 +0800

    eat and breathe function

--- classes.py
+++ classes.py
@@ -1,5 +1,14 @@
-def add(x, y):
-    return x + y
+# def add(x, y):
+#     return x + y
 
-sum = add(4, 5)
-print(sum)
+# sum = add(4, 5)
+# print(sum)
+
+class Animal:
+    def eat(self):
+        print("Eating...")
+    def breathe(self):
+        print("Breathhing...")
+
+animal1 = Animal()
+animal1.eat()
