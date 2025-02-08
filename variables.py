6be518b9337b5a5897d0a64c7304e4bb294c402a
YamiProgrammer <ticmanclarence@gmail.com>
2025-02-08 23:39:43 +0800

    input for user

--- variables.py
+++ variables.py
@@ -1,8 +1,11 @@
-x = 4
-y = 5
-name = "Clarence"
-father_name = "Ticman"
-is_student = True
-age = 19
+# x = 4
+# y = 5
+# name = "Clarence"
+# father_name = "Ticman"
+# is_student = True
+# age = 19
 
-print(name + " " + father_name + " " + str(age) + " years old")
+# print(name + " " + father_name + " " + str(age) + " years old")
+
+name = input("Enter your name: ")
+print("Hello " + name)
