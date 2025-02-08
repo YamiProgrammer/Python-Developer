d49d8f67792c170d86dec6588b19651c11afe7b3
YamiProgrammer <ticmanclarence@gmail.com>
2025-02-09 00:32:18 +0800

    using sum with strip in forloop

--- files.py
+++ files.py
@@ -2,7 +2,10 @@ file = open("Hello.txt", "r")
 # a - append, r - read, w - write
 # file.write("\nHello Adamson")
 numbers = file.readlines()
+
+sum = 0
 for number in numbers:
-    print(number.strip())
+    sum = sum + int(number.strip())
+    print(sum)
 
 file.close()
