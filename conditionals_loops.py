1e4df3e1d64983526e5accaedd0115506edaa418
YamiProgrammer <ticmanclarence@gmail.com>
2025-02-09 00:00:08 +0800

    Guessing Game

--- conditionals_loops.py
+++ conditionals_loops.py
@@ -12,5 +12,20 @@
 #     print(num)
 #     num = num + 1
 
-for num in range(10):
-    print(num)
+# for num in range(10):
+#     print(num)
+
+#Guessing game
+
+secret_number = 4
+
+guess = int(input("Enter your guess: "))
+
+while guess != secret_number:
+    if guess < secret_number:
+        print("Guess too low")
+    else:
+        print ("Guess too high")
+    guess = int(input("Enter your guess: "))
+
+print("Congratulations you have won") 
