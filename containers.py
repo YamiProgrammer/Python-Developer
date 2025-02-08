b5f63117e8bcf1a95ad710af84f7eaf20863a2b5
YamiProgrammer <ticmanclarence@gmail.com>
2025-02-09 00:14:42 +0800

    access different elements in dictionary

--- containers.py
+++ containers.py
@@ -9,15 +9,23 @@ combo = ["Clarence", 2, 3]
 
 # print(numbers[1])
 
-numbers.append(4)
-print(numbers)
-numbers.insert(1, 100)
-print(numbers)
-numbers.extend([300, 400, 25])
-print(numbers)
-numbers.sort()
-print(numbers)
-numbers.reverse()
-print(numbers)
-numbers.remove(400)
-print(numbers)
+# numbers.append(4)
+# print(numbers)
+# numbers.insert(1, 100)
+# print(numbers)
+# numbers.extend([300, 400, 25])
+# print(numbers)
+# numbers.sort()
+# print(numbers)
+# numbers.reverse()
+# print(numbers)
+# numbers.remove(400)
+# print(numbers)
+
+alphaNum = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3
+}
+
+print(alphaNum["Two"])
