5e196c8afeb8d0ad34320d8d8956376ebc09d6e0
YamiProgrammer <ticmanclarence@gmail.com>
2025-02-08 23:51:20 +0800

    while loop

--- conditionals_loops.py
+++ conditionals_loops.py
@@ -1,6 +1,12 @@
-age = 23
+# age = 23
 
-if age > 20:
-    print("You are older than 20")
-else: 
-    print("You are younger than 20")
+# if age > 20:
+#     print("You are older than 20")
+# else: 
+#     print("You are younger than 20")
+
+num = 0
+
+while num < 10:
+    print(num)
+    num = num + 1
