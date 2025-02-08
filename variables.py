786569f35fb53c66f3c7676749c7fb0a19d479df
YamiProgrammer <ticmanclarence@gmail.com>
2025-02-08 23:43:35 +0800

    Comment + input + int + string

--- variables.py
+++ variables.py
@@ -1,11 +1,20 @@
+#Step 1
 # x = 4
 # y = 5
 # name = "Clarence"
 # father_name = "Ticman"
 # is_student = True
 # age = 19
-
 # print(name + " " + father_name + " " + str(age) + " years old")
 
-name = input("Enter your name: ")
-print("Hello " + name)
+#Step 2
+# name = input("Enter your name: ")
+# print("Hello " + name)
+
+#Step3
+# Ask user for age and tell them their in five years
+age = input("Enter your age: ")
+
+age_in_five_years = int(age) + 5
+
+print("You will be " + str(age_in_five_years) + " years old in five years")
